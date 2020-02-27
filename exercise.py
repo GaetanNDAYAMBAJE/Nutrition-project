9414158d3654ef41c7f3313ee4a153f123865c14
Gaetan NDAYAMBAJE <gaetandeo@gmail.com>
2020-02-27 16:45:08 +0200

    my second project

---
+++ exercise.py
@@ -0,0 +1,22 @@
+#exercise 1 display by indexes
+# number=[34,89,97,65,100]
+# print(number[0])
+# print(number[1])
+# print(number[2])
+# print(number[3])
+# print(number[4])
+
+#exercise 2 append or add
+# cars=["Toyota","Rav4","Colorra"]
+# cars.append("Honda")
+# print(cars)
+
+#exercise 3 reverse
+# iterms=[45,21,76,89]
+# iterms.reverse()
+# print(iterms)
+
+#exercise 4 length
+cars=['Toyota', 'Rav4', 'Colorra', 'Honda']
+x=len(cars)
+print(x)
