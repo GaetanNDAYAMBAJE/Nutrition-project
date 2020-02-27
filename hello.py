377cbe2f33d404fb250a60eb94d77cc6b7e2155c
Gaetan NDAYAMBAJE <gaetandeo@gmail.com>
2020-02-27 16:28:58 +0200

    my First project

---
+++ hello.py
@@ -0,0 +1,51 @@
+#print("hello")
+#we use this in terms of add single element
+#fruits={"orange","cherry","banana","berry"}
+#fruits.add("avocados")
+#print(fruits)
+#we use this way for add many element
+#fruits={"orange","cherry","banana","berry"}
+#fruits.update(["avocados","watermelon","pineple"])
+#print(fruits)
+#use this to combine two set
+#fruits={"orange","cherry","banana","berry"}
+#vegetables={"carrots","cabbages"}
+#fruits.update(vegetables)
+#print(fruits)
+#Dictionaires
+#car={
+ #   "brand":"ford",
+  #  "model":"mustang",
+   # "year":1967
+#}
+#use one of this print
+#print(car)
+#print(car['brand'])
+#print(car.get('model'))
+
+#person=[{
+   # "first name":"Gaetan",
+    #"last name":"Deo",
+    #"year":30
+#},
+ #{
+  #  "first name":"Alexis",
+   # "last name":"mbonyi",
+    #"year":23
+#},
+#{
+ #   "first name":"Birori",
+  #  "last name":"patrick",
+  #  "year":43
+#}
+#]
+#print(person[2]['first name'])
+
+
+#array
+#cars=["Ford","Volvo","BMW"]
+#print(cars)
+#remove
+cars=["Ford","Volvo","BMW"]
+cars.pop(1)
+print(cars)
